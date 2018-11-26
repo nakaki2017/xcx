de34e3ad1d6c82b12bf5970d5d20f38b5a44e5fa
susie <azusafff@gmail.com>
2018-11-26 22:33:05 +0800

    转盘

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  appId: 'wxa9a5773d3ee56a0b',
+}
---
+++ utils/xiaojueding.js
@@ -0,0 +1,437 @@
+module.exports = [
+   {
+      id: 0,
+      option: '代码review',
+      awards: [
+         {
+            name: "赵雅博",
+            color: '#EE534F'
+         },
+         {
+            name: "闫明昌",
+            color: '#DA70D6'
+         },
+         {
+            name: "杨恒",
+            color: '#FFA827'
+         },
+         {
+            name: "王静文",
+            color: '#5C6BC0'
+         },
+         {
+            name: "陈春兰",
+            color: '#42A5F6'
+         },
+         {
+            name: "李晓宇",
+            color: '#66BB6A'
+         },
+         {
+            name: "王万荣",
+            color: '#CD5555'
+         },
+         {
+            name: "程世豪",
+            color: '#AA47BC'
+         },
+         {
+            name: "王荣",
+            color: '#EC407A'
+          },
+          {
+            name: "彭莉丽",
+            color: '#0000E3'
+          },
+          {
+            name: "张灿",
+            color: '#00A600'
+          },
+          {
+            name: "丁威",
+            color: '#EEAEEE'
+          },
+          {
+            name: "王银哲",
+            color: '#EE3B3B'
+          },
+          {
+            name: "朱启良",
+            color: '#FFFF00'
+          },
+          {
+            name: "黄金辉",
+            color: '#B2DFEE'
+          }
+      ]
+   },
+   {
+      id: 2,
+      option: '中午吃什么？',
+      awards: [
+         {
+            name: "黄焖鸡米饭",
+            color: '#FFA827'
+         },
+         {
+            name: "拌面扁肉",
+            color: '#AA47BC'
+         },
+         {
+            name: "自己多少斤心里没点数",
+            color: '#42A5F6'
+         },
+         {
+            name: "寿司",
+            color: '#00CED1'
+         },
+         {
+            name: "蛋炒饭",
+            color: '#66BB6A'
+         },
+         {
+            name: "兰州拉面",
+            color: '#FFC928'
+         },
+         {
+            name: "沙县小吃",
+            color: '#FFA500'
+         },
+         {
+            name: "麻辣烫",
+            color: '#FF4500'
+         },
+         {
+            name: "老坛酸菜牛肉面",
+            color: '#FFB6C1'
+         }
+      ]
+   },
+   {
+      id: 3,
+      option: '抽取狼人杀身份牌',
+      awards: [
+         {
+            index: 0,
+            name: "预言家",
+            color: '#EE534F'
+         },
+         {
+            index: 1,
+            name: "狼人",
+            color: '#FF7F50'
+         },
+         {
+            index: 2,
+            name: "村民",
+            color: '#FFC928'
+         },
+         {
+            index: 3,
+            name: "狼人",
+            color: '#66BB6A'
+         },
+         {
+            index: 4,
+            name: "村民",
+            color: '#42A5F6'
+         },
+         {
+            index: 5,
+            name: "狼人",
+            color: '#AA47BC'
+         },
+         {
+            index: 6,
+            name: "村民",
+            color: '#EC407A'
+         },
+         {
+            index: 7,
+            name: "狼人",
+            color: '#EE534F'
+         },
+         {
+            index: 8,
+            name: "村民",
+            color: '#FFC928'
+         },
+         {
+            index: 9,
+            name: "猎人",
+            color: '#66BB6A'
+         },
+         {
+            index: 10,
+            name: "女巫",
+            color: '#42A5F6'
+         },
+         {
+            index: 11,
+            name: "丘比特",
+            color: '#5C6BC0'
+         },
+         {
+            index: 12,
+            name: "白痴",
+            color: '#FFC928'
+         }
+      ]
+   },
+   {
+      id: 4,
+      option: '是什么激励我努力上班？',
+      awards: [
+         {
+            name: "穷",
+            color: '#EE534F'
+         },
+         {
+            name: "为了房子",
+            color: '#FF7F50'
+         },
+         {
+            name: "穷",
+            color: '#FFC928'
+         },
+         {
+            name: "为了男人",
+            color: '#66BB6A'
+         },
+         {
+            name: "穷",
+            color: '#42A5F6'
+         },
+         {
+            name: "为了女人",
+            color: '#5C6BC0'
+         },
+         {
+            name: "穷",
+            color: '#AA47BC'
+         },
+         {
+            name: "为了孩子",
+            color: '#EC407A'
+         }
+      ]
+   },
+   {
+      id: 5,
+      option: '我该答应谁的约会？',
+      awards: [
+         {
+            name: "胡歌",
+            color: '#EE534F'
+         },
+         {
+            name: "黄渤",
+            color: '#FF7F50'
+         },
+         {
+            name: "陈伟霆",
+            color: '#FFC928'
+         },
+         {
+            name: "张艺兴",
+            color: '#66BB6A'
+         },
+         {
+            name: "岳云鹏",
+            color: '#42A5F6'
+         },
+         {
+            name: "鹿晗",
+            color: '#5C6BC0'
+         },
+         {
+            name: "陈赫",
+            color: '#AA47BC'
+         },
+         {
+            name: "吴彦祖",
+            color: '#EC407A'
+         },
+         {
+            name: "王思聪",
+            color: '#EE534F'
+         },
+         {
+            name: "薛之谦",
+            color: '#FFA827'
+         },
+         {
+            name: "王宝强",
+            color: '#FFC928'
+         },
+         {
+            name: "吴亦凡",
+            color: '#66BB6A'
+         },
+         {
+            name: "宋小宝",
+            color: '#42A5F6'
+         },
+         {
+            name: "刘德华",
+            color: '#5C6BC0'
+         },
+         {
+            name: "杜海涛",
+            color: '#AA47BC'
+         },
+         {
+            name: "何炅",
+            color: '#EC407A'
+         },
+         {
+            name: "张杰",
+            color: '#C71585'
+         },
+      ]
+   },
+   {
+      id: 6,
+      option: '周末做什么好？',
+      awards: [
+         {
+            name: "逛街",
+            color: '#EE534F'
+         },
+         {
+            name: "睡觉",
+            color: '#FF7F50'
+         },
+         {
+            name: "吃大餐",
+            color: '#FFC928'
+         },
+         {
+            name: "约会",
+            color: '#66BB6A'
+         },
+         {
+            name: "看电影",
+            color: '#42A5F6'
+         },
+         {
+            name: "游乐场",
+            color: '#5C6BC0'
+         },
+         {
+            name: "爬山",
+            color: '#EC407A'
+         },
+         {
+            name: "健身房",
+            color: '#66BB6A'
+         },
+
+      ]
+   },
+   {
+      id: 7,
+      option: '十一国庆去哪玩',
+      awards: [
+         {
+            name: "凤凰古城",
+            color: '#EE534F'
+         },
+         {
+            name: "张家界",
+            color: '#FF7F50'
+         },
+         {
+            name: "乌镇",
+            color: '#FFC928'
+         },
+         {
+            name: "鼓浪屿",
+            color: '#66BB6A'
+         },
+         {
+            name: "千岛湖",
+            color: '#42A5F6'
+         },
+         {
+            name: "稻城",
+            color: '#5C6BC0'
+         },
+         {
+            name: "大理",
+            color: '#AA47BC'
+         },
+         {
+            name: "丽江",
+            color: '#EC407A'
+         },
+
+      ]
+   },
+   {
+      id: 8,
+      option: '我要买什么品牌的手机',
+      awards: [
+         {
+            name: "华为",
+            color: '#EE534F'
+         },
+         {
+            name: "OPPO",
+            color: '#FF7F50'
+         },
+         {
+            name: "一加",
+            color: '#FFC928'
+         },
+         {
+            name: "vivo",
+            color: '#66BB6A'
+         },
+         {
+            name: "酷派",
+            color: '#42A5F6'
+         },
+         {
+            name: "诺基亚",
+            color: '#5C6BC0'
+         },
+         {
+            name: "小米",
+            color: '#AA47BC'
+         },
+         {
+            name: "努比亚",
+            color: '#EC407A'
+         },
+         {
+            name: "三星",
+            color: '#EE534F'
+         },
+         {
+            name: "金立",
+            color: '#FF7F50'
+         },
+         {
+            name: "魅族",
+            color: '#FFC928'
+         },
+         {
+            name: "iPhone",
+            color: '#66BB6A'
+         },
+         {
+            name: "锤子",
+            color: '#42A5F6'
+         },
+         {
+            name: "HTC",
+            color: '#5C6BC0'
+         },
+         {
+            name: "索尼",
+            color: '#AA47BC'
+         }
+      ]
+   }
+]
